98cd18d053e611e5d9871df6c1764a16810b1b71
anshul 524 <anshul0524@gmail.com>
2024-07-11 12:55:44 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+const navbtn = () => {
+    document.getElementById("nav-menu").classList.toggle("show-nav")
+    document.body.classList.toggle("overflow-hidden")
+}
+// function navbarHandler() {
+//     document.getElementById("myTopnav").classList.toggle('show_navbar');
+// }
